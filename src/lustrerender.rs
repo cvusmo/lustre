0a71cc991cbecedab7c6f1457937fa1814ea5cf4
cvusmo <blacksheepcosmo@gmail.com>
2025-02-22 05:10:38 -0600

    0.1.1-122-p: successful graphics pipeline.

--- src/lustrerender.rs
+++ src/lustrerender.rs
@@ -5,35 +5,32 @@
 use std::sync::Arc;
 
 use image::{ImageBuffer, Rgba};
-use vulkano::buffer::{Buffer, BufferCreateInfo, BufferUsage};
+use vulkano::buffer::{Buffer, BufferContents, BufferCreateInfo, BufferUsage};
 use vulkano::command_buffer::allocator::{
     StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo,
 };
 use vulkano::command_buffer::{
-    AutoCommandBufferBuilder, CommandBufferUsage, CopyImageToBufferInfo,
-}; // ClearColorImageInfo
-use vulkano::descriptor_set::allocator::StandardDescriptorSetAllocator;
-use vulkano::descriptor_set::{DescriptorSet, WriteDescriptorSet};
-use vulkano::device::{Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInfo, QueueFlags};
-use vulkano::format::Format; //ClearColorValue
+    AutoCommandBufferBuilder, CommandBufferUsage, CopyImageToBufferInfo, RenderPassBeginInfo,
+    SubpassBeginInfo, SubpassContents,
+};
+use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
+use vulkano::format::Format;
 use vulkano::image::view::ImageView;
 use vulkano::image::{Image, ImageCreateInfo, ImageType, ImageUsage};
 use vulkano::instance::{Instance, InstanceCreateFlags, InstanceCreateInfo};
 use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator};
-use vulkano::pipeline::compute::ComputePipelineCreateInfo;
+use vulkano::pipeline::graphics::color_blend::{ColorBlendAttachmentState, ColorBlendState};
+use vulkano::pipeline::graphics::input_assembly::InputAssemblyState;
+use vulkano::pipeline::graphics::multisample::MultisampleState;
+use vulkano::pipeline::graphics::rasterization::RasterizationState;
+use vulkano::pipeline::graphics::vertex_input::{Vertex, VertexDefinition};
+use vulkano::pipeline::graphics::viewport::{Viewport, ViewportState};
+use vulkano::pipeline::graphics::GraphicsPipelineCreateInfo;
 use vulkano::pipeline::layout::PipelineDescriptorSetLayoutCreateInfo;
-use vulkano::pipeline::{
-    ComputePipeline, Pipeline, PipelineBindPoint, PipelineLayout, PipelineShaderStageCreateInfo,
-};
+use vulkano::pipeline::{GraphicsPipeline, PipelineLayout, PipelineShaderStageCreateInfo};
+use vulkano::render_pass::{Framebuffer, FramebufferCreateInfo, Subpass};
 use vulkano::sync::{self, GpuFuture};
 use vulkano::VulkanLibrary;
-
-mod cs {
-    vulkano_shaders::shader! {
-        ty: "compute",
-        path: "src/shaders/mandelbrot.comp"
-    }
-}
 
 pub fn lustrerender() {
     // Initialization
@@ -69,36 +66,12 @@ pub fn lustrerender() {
                 queue_family_index,
                 ..Default::default()
             }],
-            enabled_extensions: DeviceExtensions {
-                khr_storage_buffer_storage_class: true,
-                ..DeviceExtensions::empty()
-            },
             ..Default::default()
         },
     )
     .expect("failed to create device");
 
     let queue = queues.next().unwrap();
-
-    // Load shaders
-    let shader = cs::load(device.clone()).expect("failed to create shader module");
-    let cs = shader.entry_point("main").unwrap();
-    let stage = PipelineShaderStageCreateInfo::new(cs);
-    let layout = PipelineLayout::new(
-        device.clone(),
-        PipelineDescriptorSetLayoutCreateInfo::from_stages([&stage])
-            .into_pipeline_layout_create_info(device.clone())
-            .unwrap(),
-    )
-    .unwrap();
-
-    // Compute Pipelines
-    let compute_pipeline = ComputePipeline::new(
-        device.clone(),
-        None,
-        ComputePipelineCreateInfo::stage_layout(stage, layout),
-    )
-    .expect("failed to create compute pipeline");
 
     let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
 
@@ -109,37 +82,15 @@ pub fn lustrerender() {
             image_type: ImageType::Dim2d,
             format: Format::R8G8B8A8_UNORM,
             extent: [1024, 1024, 1],
-            usage: ImageUsage::STORAGE | ImageUsage::TRANSFER_SRC | ImageUsage::TRANSFER_DST,
+            usage: ImageUsage::COLOR_ATTACHMENT
+                | ImageUsage::TRANSFER_SRC
+                | ImageUsage::TRANSFER_DST,
             ..Default::default()
         },
         AllocationCreateInfo {
             memory_type_filter: MemoryTypeFilter::PREFER_DEVICE,
             ..Default::default()
         },
-    )
-    .unwrap();
-
-    // Create view
-    let view = ImageView::new_default(image.clone()).unwrap();
-
-    // Create the descriptor set allocators
-    let descriptor_set_allocator = Arc::new(StandardDescriptorSetAllocator::new(
-        device.clone(),
-        Default::default(),
-    ));
-
-    let pipeline_layout = compute_pipeline.layout();
-    let descriptor_set_layouts = pipeline_layout.set_layouts();
-    let descriptor_set_layout_index = 0;
-    let descriptor_set_layout = descriptor_set_layouts
-        .get(descriptor_set_layout_index)
-        .unwrap();
-
-    let descriptor_set = DescriptorSet::new(
-        descriptor_set_allocator.clone(), // Pass an Arc, not a reference.
-        descriptor_set_layout.clone(),
-        [WriteDescriptorSet::image_view(0, view.clone())], // 0 is the binding
-        [],
     )
     .unwrap();
 
@@ -159,7 +110,144 @@ pub fn lustrerender() {
     )
     .expect("failed to create buffer");
 
-    // Create the command buffer allocator, wrapped in an Arc.
+    // Create the Vertex
+    #[derive(BufferContents, Vertex)]
+    #[repr(C)]
+    struct MainVertex {
+        #[format(R32G32_SFLOAT)]
+        position: [f32; 2],
+    }
+
+    let vertex1 = MainVertex {
+        position: [-0.5, -0.5],
+    };
+
+    let vertex2 = MainVertex {
+        position: [0.0, 0.5],
+    };
+
+    let vertex3 = MainVertex {
+        position: [0.5, -0.25],
+    };
+
+    // Triangle
+    let vertex_buffer = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::VERTEX_BUFFER,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
+                | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+            ..Default::default()
+        },
+        vec![vertex1, vertex2, vertex3].into_iter(),
+    )
+    .unwrap();
+
+    // Single Render Pass
+    let render_pass = vulkano::single_pass_renderpass!(device.clone(),
+        attachments: {
+            color: {
+                format: Format::R8G8B8A8_UNORM,
+                samples: 1,
+                load_op: Clear,
+                store_op: Store,
+            },
+        },
+        pass: {
+            color: [color],
+            depth_stencil: {},
+        },
+    )
+    .unwrap();
+
+    // Creating Framebuffers
+    let view = ImageView::new_default(image.clone()).unwrap();
+    let framebuffer = Framebuffer::new(
+        render_pass.clone(),
+        FramebufferCreateInfo {
+            attachments: vec![view],
+            ..Default::default()
+        },
+    )
+    .unwrap();
+
+    // Shaders
+    mod vs {
+        vulkano_shaders::shader! {
+            ty: "vertex",
+            path: "src/shaders/vertex.comp",
+        }
+    }
+
+    mod fs {
+        vulkano_shaders::shader! {
+            ty: "fragment",
+            path: "src/shaders/fragment.comp",
+        }
+    }
+
+    let vs = vs::load(device.clone()).expect("failed to create vertex shader module");
+    let fs = fs::load(device.clone()).expect("failed to create fragment shader module");
+
+    // Create viewport
+    let viewport = Viewport {
+        offset: [0.0, 0.0],
+        extent: [1024.0, 1024.0],
+        depth_range: 0.0..=1.0,
+    };
+
+    // Create pipeline
+    let pipeline = {
+        let vs = vs.entry_point("main").unwrap();
+        let fs = fs.entry_point("main").unwrap();
+
+        let vertex_input_state = MainVertex::per_vertex().definition(&vs).unwrap();
+
+        let stages = [
+            PipelineShaderStageCreateInfo::new(vs),
+            PipelineShaderStageCreateInfo::new(fs),
+        ];
+
+        let layout = PipelineLayout::new(
+            device.clone(),
+            PipelineDescriptorSetLayoutCreateInfo::from_stages(&stages)
+                .into_pipeline_layout_create_info(device.clone())
+                .unwrap(),
+        )
+        .unwrap();
+
+        // Create subpass
+        let subpass = Subpass::from(render_pass.clone(), 0).unwrap();
+
+        // Define our graphics pipeline
+        GraphicsPipeline::new(
+            device.clone(),
+            None,
+            GraphicsPipelineCreateInfo {
+                stages: stages.into_iter().collect(),
+                vertex_input_state: Some(vertex_input_state),
+                input_assembly_state: Some(InputAssemblyState::default()),
+                viewport_state: Some(ViewportState {
+                    viewports: [viewport].into_iter().collect(),
+                    ..Default::default()
+                }),
+                rasterization_state: Some(RasterizationState::default()),
+                multisample_state: Some(MultisampleState::default()),
+                color_blend_state: Some(ColorBlendState::with_attachment_states(
+                    subpass.num_color_attachments(),
+                    ColorBlendAttachmentState::default(),
+                )),
+                subpass: Some(subpass.into()),
+                ..GraphicsPipelineCreateInfo::layout(layout)
+            },
+        )
+        .unwrap()
+    };
+
+    // Drawing
     let cb_allocator = Arc::new(StandardCommandBufferAllocator::new(
         device.clone(),
         StandardCommandBufferAllocatorCreateInfo::default(),
@@ -176,43 +264,44 @@ pub fn lustrerender() {
     // Determine number of work groups to dispatch
     // To cover a 1024x1024 image: 1024 / 8 = 128
     // 128 in x and 128 in y directions
-    let work_group_counts = [128, 128, 1];
+    //let work_group_counts = [128, 128, 1];
 
-    // Memory transfer operation
-    // 1. Clear image
-    //cb_builder
-    //.clear_color_image(ClearColorImageInfo {
-    //clear_value: ClearColorValue::Float([0.0, 0.0, 1.0, 1.0]),
-    //..ClearColorImageInfo::image(image.clone())
-    //})
-    //.unwrap();
-
-    // 2. Bind Compute pipeline && descriptor set
-    cb_builder
-        .bind_pipeline_compute(compute_pipeline.clone())
-        .unwrap()
-        .bind_descriptor_sets(
-            PipelineBindPoint::Compute,
-            compute_pipeline.layout().clone(),
-            descriptor_set_layout_index as u32,
-            descriptor_set,
-        )
-        .unwrap();
-
-    // 3. Wrap the call to dispatch in an unsafe block:
     unsafe {
-        cb_builder.dispatch(work_group_counts).unwrap();
+        cb_builder
+            .begin_render_pass(
+                RenderPassBeginInfo {
+                    clear_values: vec![Some([0.0, 0.0, 1.0, 1.0].into())],
+                    ..RenderPassBeginInfo::framebuffer(framebuffer)
+                },
+                SubpassBeginInfo {
+                    contents: SubpassContents::Inline,
+                    ..Default::default()
+                },
+            )
+            .unwrap()
+            .bind_pipeline_graphics(pipeline)
+            .unwrap()
+            .bind_vertex_buffers(0, vertex_buffer)
+            .unwrap()
+            .draw(
+                3, 1, 0, 0, // 3 is the number of vertices, 1 is the number of instances
+            )
+            .unwrap()
+            .end_render_pass(Default::default())
+            .unwrap()
+            .copy_image_to_buffer(CopyImageToBufferInfo::image_buffer(image, buf.clone()))
+            .unwrap();
     }
 
-    // 4. Copy image to buffer
-    cb_builder
-        .copy_image_to_buffer(CopyImageToBufferInfo::image_buffer(
-            image.clone(),
-            buf.clone(),
-        ))
-        .unwrap();
+    // 3. Wrap the call to dispatch in an unsafe block:
+    //unsafe {
+    //cb_builder.dispatch(work_group_counts).unwrap();
+    //}
 
-    // Build the command buffer.
+    //cb_builder
+    //.copy_image_to_buffer(CopyImageToBufferInfo::image_buffer(image, buf.clone()))
+    //.unwrap();
+
     let command_buffer = cb_builder.build().unwrap();
 
     // Submit the command buffer.
@@ -221,7 +310,6 @@ pub fn lustrerender() {
         .unwrap()
         .then_signal_fence_and_flush()
         .unwrap();
-
     future.wait(None).unwrap();
 
     // Generate image
